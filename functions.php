2c00fb1ea860eb1e8105639a56a6aeca99d18cf6
Greg Bastianelli <greg@gerrg.com>
2020-12-05 23:31:13 -0500

    add hearts post meta to rest api

--- functions.php
+++ functions.php
@@ -1,5 +1,35 @@
 <?php
 
+add_action("rest_api_init", "gregbastianelli_register_post_meta");
+/**
+ * Add hearts post meta to API
+ */
+function gregbastianelli_register_post_meta()
+{
+	register_post_meta( 'post', 'hearts', array(
+        'show_in_rest' => true,
+        'single' => true,
+        'type' => 'integer',
+    ) );
+}
+
+add_action( 'graphql_register_types', 'gregbastianelli_add_hearts_post_meta_to_graphql');
+/**
+ * Add hearts post meta to graphQL
+ */
+function gregbastianelli_add_hearts_post_meta_to_graphql() {
+  register_graphql_field( 'Post', 'hearts', [
+    'type' => 'Integer',
+    'description' => __( 'Get number of times a post has been hearted', 'gregbastianelli' ),
+    'resolve' => function( \WPGraphQL\Model\Post $post, $args, $context, $info ) {
+      $hearts = get_post_meta( $post->ID, 'hearts', true );
+		return (! empty($hearts)) ? $hearts : 0;
+    }
+  ] );
+}
+
+
+
 add_action("wp_body_open", "gregbastianelli_redirect_to_frontend", 1);
 
 function gregbastianelli_redirect_to_frontend()
